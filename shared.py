a059842411e7589d29b5eea4079838e679b92615
Feroze Mohideen <feroze@porter.run>
2023-11-06 10:32:17 -0500

    Update shared.py

--- shared.py
+++ shared.py
@@ -20,8 +20,8 @@ def memory_consumer():
     """
     allocates a variable with a random size
     """
-    rand_one = random.randint(8, 12)
-    rand_two = random.randint(8, 12)
+    rand_one = random.randint(80, 120)
+    rand_two = random.randint(80, 120)
     placeholder = ["bar" for _ in range((1000000 * (rand_one + rand_two)))]
     del placeholder
 
